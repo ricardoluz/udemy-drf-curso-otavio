c8564186ed105317780e2e86b97a5d0e6c2c106a
ricardoluz <contato@ricardoluz.com.br>
2024-08-04 07:56:21 -0300

    Uso de inicial autenticação

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -4,11 +4,16 @@ from rest_framework import status
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 
 from ..models import Recipe
 from tag.models import Tag
 
 from ..serializers import RecipeSerializer, TagSerializer
+
+from ..permissions import IsOwner
+from django.shortcuts import get_object_or_404
 
 
 class RecipeAPIv2Pagination(PageNumberPagination):
@@ -19,12 +24,55 @@ class RecipeApiv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+    http_method_names = ["get", "options", "head", "patch", "delete"]
+
+    # Permite a apenas leitura quando não estiver autenticado.
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # Sobrescrevendo o método create para guardar o author.
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # self.perform_create(serializer)
+        # Linha alterada.
+        # TODO: Retirar o [is_published=True] do save.
+        serializer.save(author=request.user, is_published=True)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
+
+    # ALterar o permissionamento para realizar alterações.
+    def get_permissions(self):
+        if self.request.method in ["PATCH", "DELETE"]:
+            return [IsOwner()]
+
+        return super().get_permissions()
+
+    # Sobrescrever o método de obtenção do objeto.
+    # De forma a validar as permissões de uso do objeto
+    # quando ele for chamado.
+    def get_object(self):
+        pk = self.kwargs.get("pk")
+        obj = pk = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    # Sobrecreve
 
     # Exemplo de personalização de um método que está embutido na  ...APIView
-    # Sendo sobreescrito.
+    # Sendo sobrescrito.
     def partial_update(self, request, *args, **kwargs):
-        pk = kwargs.get("pk")
-        recipe = self.get_queryset().filter(pk=pk).first()
+        # pk = kwargs.get("pk")
+        # recipe = self.get_queryset().filter(pk=pk).first()
+        recipe = self.get_object()
         serializer = RecipeSerializer(
             instance=recipe,
             many=False,
